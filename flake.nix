90072c325ba5f2222bd7a4718e4c22f3ffc8ca51
skyer <skyer@skyer.tw>
2024-05-21 14:12:34 +0000

    Add home manager

--- flake.nix
+++ flake.nix
@@ -2,13 +2,24 @@
   description = "Skyer's nixos flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, ...}@inputs: {
+  outputs = { self, nixpkgs, home-manager, ...}@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+
+	  home-manager.users.skyer = import ./home.nix;
+	}
       ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "skyer";
+  home.homeDirectory = "/home/skyer";
+
+  # 直接将当前文件夹的配置文件，链接到 Home 目录下的指定位置
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # 递归将某个文件夹中的文件，链接到 Home 目录下的指定位置
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # 递归整个文件夹
+  #   executable = true;  # 将其中所有文件添加「执行」权限
+  # };
+
+  # 直接以 text 的方式，在 nix 配置文件中硬编码文件内容
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  home.packages = with pkgs; [
+    neofetch
+    zip
+    xz
+    unzip
+    p7zip
+    ripgrep
+    jq
+    fd
+    fzf
+    lazygit
+    jq
+    go
+    rustup
+    python3
+    bottom
+    lm_sensors
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Skyer";
+    userEmail = "skyer.tw@gmail.com";
+  };
+
+  home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
+}
